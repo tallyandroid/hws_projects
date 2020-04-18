5412d1b825eb58455e0182900e11648196eb1179
Christian Tisby <cduane82@hotmail.com>
2020-04-18 00:30:14 -0400

    adding operators2

---
+++ operators2.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+// Comparisons
+c > 3
+c >= 3
+c > 4
+c < 4
+
+
+var name = "Tim McGraw"
+name == "TIM McGraw"
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
