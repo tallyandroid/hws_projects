bc85cfc73cc90c3fcb8f21c880d4e28830e9be22
Christian Tisby <cduane82@hotmail.com>
2020-04-18 00:11:52 -0400

    adding data types

---
+++ data.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//String
+var name: String = "Tim McGraw"
+
+//Integers
+let age = 37
+var year = 2020
+
+//Double
+var latitude: Double
+latitude = 36.166667
+
+
+//Float
+var longitude: Float
+longitude = -1286.783333
+
+//Boolean
+var stayAtHome = true
+var coronaVirusIsReal: Bool
+coronaVirusIsReal = true
+
+
+//type Inference
+var loveMyJob = false
+var lastName = "Tisby"
+
