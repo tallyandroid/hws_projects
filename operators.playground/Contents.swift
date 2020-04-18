48850a3a8c021e066edd3628316e5c00900b85c1
Christian Tisby <cduane82@hotmail.com>
2020-04-18 00:18:28 -0400

    adding operators

---
+++ operators.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//var a = 10
+//a = a + 1
+//a = a - 1
+//a = a * a
+//
+//var b = 10
+//b += 10
+//b -= 10
+//
+
+
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+
+
+var both = name1 + " and " + name2
+
+9 % 3
+9 % 2
+
