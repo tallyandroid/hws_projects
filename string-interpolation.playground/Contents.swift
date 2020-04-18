f34f9086c126fe68de32e2eee78b00eceacd4d86
Christian Tisby <cduane82@hotmail.com>
2020-04-18 00:53:38 -0400

    adding string interpolation

---
+++ string-interpolation.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var name = "Tim McGraw"
+var age = 25
+var latitude = 36.166667
+
+"Your name is \(name), your age is \(age), and your latitude is \(latitude)."
+
+"Your age is \(age) years old.  In another \(age) years old you will be \(age * 2)"
