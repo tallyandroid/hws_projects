4233ba193092e779ed26afc09cd9b6749b771f95
Christian Tisby <cduane82@hotmail.com>
2020-04-20 02:26:17 -0400

    adding arrays2

---
+++ arrays2.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+//var songs: [String] = []
+//songs[0] = "Shake It Off"
+//
+//var songs2 = [String]()
+//songs2[0] = "Players Ball"
+
+var songs1 = ["Shake it Off", "You Belong With Me", "Love Story"]
+var songs3 = ["Today was a Fairytale", "Welcome to New York", "Fifteen"]
+
+var both = songs1 + songs3
+both += ["Everything has Changed"]
+print(both)
