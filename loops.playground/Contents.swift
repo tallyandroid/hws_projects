85d33de8c06661b9a4aff27c67e014d7c3744160
Christian Tisby <cduane82@hotmail.com>
2020-04-20 02:57:06 -0400

    adding loops

---
+++ loops.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+for i in 1...10 {
+    print("\(i) x 10 is \(i * 10)")
+}
+
+var str = "Fakers gonna"
+
+for _ in 1...5 {
+    str += " fake"
+}
+
+print(str)
+
+//half range operator
+
+1 ..< 5
+
