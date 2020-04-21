292ed3f80e5157cfa2d8f99a68d0835a2cf971a3
Christian Tisby <cduane82@hotmail.com>
2020-04-21 02:19:23 -0400

    adding optionals

---
+++ optionals.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+func getHaterStatus(weather: String) -> String? {
+    if weather == "sunny"{
+        return nil
+    } else {
+        return "Hate"
+    }
+    
+}
+
+var status: String?
+status = getHaterStatus(weather: "rainy")
+
+if let unwrappedStatus = status {
+    // unwrappedStatus contains a non-optional string
+} else {
+    // in case you want an else block, here you go
+}
+
+func takeHaterAction(status: String) {
+    if status == "Hate" {
+        print("Hating")
+    }
+}
+
+if let haterStatus = getHaterStatus(weather: "rainy") {
+    takeHaterAction(status: haterStatus)
+}
