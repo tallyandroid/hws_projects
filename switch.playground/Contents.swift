ebf34400aef22974dc6f04da4be63eb524dbf911
Christian Tisby <cduane82@hotmail.com>
2020-04-21 02:00:17 -0400

    adding switch

---
+++ switch.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let liveAlbums = 2
+
+switch liveAlbums {
+case 0:
+    print("You're just starting out")
+    
+case 1:
+    print("You just released iTunes Live From SoHo")
+
+case 2:
+    print("You just released Speak Now World Tour")
+    
+default:
+    print("Have you done something new?")
+}
