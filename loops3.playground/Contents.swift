5369496f45e14d2916dc3cd887e5391352232963
Christian Tisby <cduane82@hotmail.com>
2020-04-21 01:02:39 -0400

    adding loops 4

---
+++ loops3.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var people = ["players", "haters", "heart-breakers", "fakers"]
+var actions = ["play", "hate", "break", "fake"]
+
+for i in 0..<people.count {
+    var str = "\(people[i]) gonna"
+    
+    for _ in 1...5 {
+        str += " \(actions[i])"
+    }
+    print(str)
+}
