84d47d758d8424f0811e42d8e32ab9bc2c424eb9
Christian Tisby <cduane82@hotmail.com>
2020-04-21 00:54:52 -0400

    adding loops2

---
+++ loops2.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var songs = ["Shake it Off", "You Belong With Me", "Look What You Made Me Do",]
+
+for song in songs {
+    print("My favorite song is \(song)")
+}
+
+var people = ["players", "haters", "heart-breakers", "fakers"]
+var actions = ["play", "hate", "break", "fake"]
+
+for i in 0...3 {
+    print("\(people[i]) gonna \(actions[i])")
+}
