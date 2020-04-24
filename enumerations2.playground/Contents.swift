e02f4aa7e2e94c0479c16f9f6a880752ef619e9d
Christian Tisby <cduane82@hotmail.com>
2020-04-23 23:54:34 -0400

    adding enumerations2

---
+++ enumerations2.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+enum WeatherType {
+    case sun
+    case cloud
+    case rain
+    case wind(speed: Int)
+    case snow
+}
+
+func getHaterStatus(weather: WeatherType) -> String? {
+    switch weather {
+    case .sun:
+        return nil
+    case .wind(let speed) where speed < 10:
+        return "meh"
+    case .cloud, .wind:
+        return "dislike"
+    case .rain, .snow:
+        return "hate"
+    }
+}
