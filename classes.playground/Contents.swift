2728406c77d9e574ac58ee86658c3322ad9c4d8f
Christian Tisby <cduane82@hotmail.com>
2020-04-24 23:03:40 -0400

    adding classes

---
+++ classes.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+class Person  {
+    var clothes: String
+    var shoes: String
+    
+    init(clothes: String, shoes: String) {
+        self.clothes = clothes
+        self.shoes = shoes
+    }
+}
