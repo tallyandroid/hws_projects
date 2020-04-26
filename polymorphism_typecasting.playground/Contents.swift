577a3e95045c16ddf5923757298e6c62afd6f541
Christian Tisby <cduane82@hotmail.com>
2020-04-25 21:07:11 -0400

    adding polumorphism_typecasting

---
+++ polymorphism_typecasting.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+class Album {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getPerformance() -> String {
+        return "The album \(name) sold lots"
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+    
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+    
+    override func getPerformance() -> String {
+        return "The studio album \(name) sold lots."
+    }
+    
+}
+
+class LiveAlbum: Album {
+    var location: String
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+    }
+    
+    override func getPerformance() -> String {
+        return "The live album \(name) sold lots."
+    }
+}
+
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studio")
+var fearless = StudioAlbum(name: "Fearless", studio: "Aimeeland Studio")
+var iTunesLive = LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+var allAlbums: [Album] = [taylorSwift, fearless, iTunesLive]
+
+for album in allAlbums {
+    print(album.getPerformance())
+}
+
