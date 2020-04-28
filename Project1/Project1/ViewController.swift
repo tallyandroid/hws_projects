a661783c44af7f829e8bac6a3c3805413f7e2271
Christian Tisby <cduane82@hotmail.com>
2020-04-27 21:56:03 -0400

    updating project1

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -12,7 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load
+            }
+           
+        }
     }
 
 
