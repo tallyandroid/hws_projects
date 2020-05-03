18c80fe52ac23a4abf1c6aaae27874e421e1a3ef
Christian Tisby <cduane82@hotmail.com>
2020-05-02 22:54:22 -0400

    adding flags and constraints

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Christian Tisby on 5/2/20.
+//  Copyright © 2020 Christian Tisby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
