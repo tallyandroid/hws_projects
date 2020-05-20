9c1fe5ac214f388222bd0ddc715af2c5bf7887de
tallyandroid <cduane82@hotmail.com>
2020-05-19 23:32:24 -0400

    updating project 5

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+            }
+        }
     }
 
 
