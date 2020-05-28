f794b1fbde38e66d2f64f90264407bb965336326
tallyandroid <cduane82@hotmail.com>
2020-05-27 23:41:15 -0400

    updating project 6b

--- project6b/project6b/ViewController.swift
+++ project6b/project6b/ViewController.swift
@@ -55,6 +55,7 @@ class ViewController: UIViewController {
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
         
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
     }
 
 
