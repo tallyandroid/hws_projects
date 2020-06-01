86c03f03dcd8b260c83b6cb87397ff161867fb3f
tallyandroid <cduane82@hotmail.com>
2020-06-01 00:13:04 -0400

    updating playground

--- big nerd ranch chapter 2.playground/Contents.swift
+++ big nerd ranch chapter 2.playground/Contents.swift
@@ -22,7 +22,8 @@ let fmStation = 91.2
 
 
 
-let countingUp = ["one", "two"]
+
+var countingUp = ["one", "two"]
 let nameByParkingSpace = [13: "Alice", 27: "Bob"]
 
 let secondElement = countingUp[1]
@@ -46,3 +47,62 @@ let floatFromLiteral = Float(3.14)
 let easyPi = 3.14
 let floatFromDouble = Float(easyPi)
 let floatingPi: Float = 3.14
+
+//Properties
+
+countingUp.count
+emptyString.isEmpty
+
+//instance method
+
+countingUp.append("three")
+
+//Optionals
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+
+reading1 = 9.8
+reading2 = 9.2
+reading3 = 9.7
+
+if let r1 = reading1,
+let r2 = reading2,
+    let r3 = reading3 {
+    let avgReading = (r1 + r2 + r3) / 3
+    print(avgReading)
+} else {
+    let errorString = "Instrument reported a reading that was nil."
+    print(errorString)
+}
+
+
+//Subscripting  dictionaries
+let space13Assignee: String? = nameByParkingSpace[13]
+let space42Assignee: String? = nameByParkingSpace[42]
+
+if let space13Assignee = nameByParkingSpace[13] {
+    print(space13Assignee)
+}
+
+
+//Enumerations
+
+enum PieType: Int {
+    case apple = 0
+    case cherry
+    case pecan
+}
+
+let favoritePie = PieType.apple
+
+let name: String
+switch favoritePie {
+case .apple:
+    name = "Apple"
+case .cherry:
+    name = "Cherry"
+case .pecan:
+    name = "Pecan"
+}
