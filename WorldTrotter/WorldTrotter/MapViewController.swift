bcd8152ad717e80582fe36cdb9b0ef472ee92b9e
tallyandroid <cduane82@hotmail.com>
2020-06-06 22:59:07 -0400

    adding ConversionViewController to WorldTrotter

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Christian Tisby on 6/6/20.
+//  Copyright © 2020 Christian Tisby. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
